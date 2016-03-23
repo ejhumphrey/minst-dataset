60e416d795b29be4b6ee996503bf3d13d6cf5bd3
Eric J. Humphrey <ejhumphrey@nyu.edu>
2016-03-23 14:27:36 -0400

    Added note distance to utils

--- minst/utils.py
+++ minst/utils.py
@@ -28,3 +28,48 @@ def generate_id(prefix, name, hash_len=8):
 def filebase(fpath):
     """Return the file's basename without an extension, e.g. 'x/y.z' -> 'y'."""
     return os.path.splitext(os.path.basename(fpath))[0]
+
+
+def note_distance(note_pair):
+    """Get the distance in semitones between two named
+    notes.
+
+    E.g.
+    (Bb1, B1) => 1
+    (C4, B4) => 11
+    (C5, Bb5) => 10
+
+    Parameters
+    ----------
+    note_pair : tuple of ints
+
+    Returns
+    -------
+    note_distance : int
+    """
+    char_map = {'C': 0, 'D': 2, 'E': 4, 'F': 5, 'G': 7, 'A': 9, 'B': 11}
+
+    def apply_modifier(val, modifier):
+        if modifier == "#":
+            return val + 1
+        elif modifier == 'b':
+            return val - 1
+        else:
+            return val
+
+    # get the chars
+    first_note = [x for x in note_pair[0]]
+    second_note = [x for x in note_pair[1]]
+
+    first_name, first_oct = first_note[0], first_note[-1]
+    first_mod = first_note[1] if len(first_note) == 3 else None
+    second_name, second_oct = second_note[0], second_note[-1]
+    second_mod = second_note[1] if len(second_note) == 3 else None
+
+    base_dist = apply_modifier(char_map[second_name], second_mod) - \
+        apply_modifier(char_map[first_name], first_mod)
+
+    oct_diff = int(second_oct) - int(first_oct)
+    base_dist += (oct_diff * 12)
+
+    return base_dist
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,10 @@
 import minst.utils
 
 
+def __test(value, expected):
+    assert value == expected
+
+
 def test_generate_id():
     def __test_hash(prefix, result, hlen):
         assert result.startswith(prefix)
@@ -13,3 +17,16 @@ def test_generate_id():
     for prefix, name, hlen in tests:
         result = minst.utils.generate_id(prefix, name, hlen)
         yield __test_hash, prefix, result, hlen
+
+
+def test_get_note_distance():
+    test_pairs = [(('Bb1', 'B1'), 1),
+                  (('C4', 'B4'), 11),
+                  (('Bb1', 'B2'), 13),
+                  (('C3', 'C4'), 12),
+                  (('F5', 'F5'), 0),
+                  (('C#3', 'C4'), 11)]
+
+    for value, expected in test_pairs:
+        result = minst.utils.note_distance(value)
+        yield __test, result, expected
