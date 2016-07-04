14452c11451dc26e28bd4487004f73b2b8933f70
Christopher Jacoby <cjacoby@museami.com>
2016-07-04 09:39:02 -0700

    a bunch of new features for editing, including:\n * mark file to come back to later\n * arrow keys to move all the markers\n * number keys to run pre-made minst.signal algorithms on the file\n * timer for each file\n * improved logging\n * argument to start at an index

--- scripts/annotate.py
+++ scripts/annotate.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 
 """
-Show how to connect to keypress events
+A GUI tool for annotating onsets in audio files.
+
+Example of how to run:
+ > python scripts/annotate.py onsets/rwc/segment_index.csv --verbose
+
+ See the README.md for more detailed information.
+
+Keys:
+
 """
 from __future__ import print_function
 import argparse
@@ -9,7 +17,9 @@ import claudio
 import logging
 import matplotlib
 import numpy as np
+import os
 import pandas as pd
+import pprint
 import sys
 import time
 
@@ -31,15 +41,15 @@ class OnsetCanvas(object):
         self.fig, self.axes = plt.subplots(nrows=2, ncols=1,
                                            figsize=(16, 6))
 
-        x, fs = claudio.read(audio_file, samplerate=22050,
+        self.x, self.fs = claudio.read(audio_file, samplerate=22050,
                              channels=1, bytedepth=2)
 
         onset_data = pd.DataFrame([]) if onset_data is None else onset_data
         self.output_file = output_file
-        self.x_max = np.abs(x).max()
-        self.trange = np.arange(0, len(x), nhop) / float(fs)
-        self.waveform = x.flatten()[::nhop]
-        self.envelope = S.log_envelope(x, fs, nhop)[::nhop]
+        self.x_max = np.abs(self.x).max()
+        self.trange = np.arange(0, len(self.x), nhop) / float(self.fs)
+        self.waveform = self.x.flatten()[::nhop]
+        self.envelope = S.log_envelope(self.x, self.fs, nhop)[::nhop]
         # self.onset_data = onset_data
         self.wave_handle = self.axes[0].plot(self.trange, self.waveform)
         self.env_handle = self.axes[1].plot(self.trange, self.envelope)
@@ -47,17 +57,25 @@ class OnsetCanvas(object):
         self.refresh_xlim()
 
         title = '' if not title else title
-        title = "{}\nx: write and close / w: write / q: close".format(title)
+        title = ("{}\nx: write and close / w: write / q: close / c: clear / "
+            "SPACE: set/delete marker\n"
+            "1: envelope onsets / 2: logcqt onsets".format(title))
         self.axes[0].set_title(title)
 
         self.set_onset_data(onset_data)
         self.fig.canvas.mpl_connect('key_press_event', self.on_key_press)
         self._alive = False
+        self._quit = False
+        self._mark_for_later = False
 
     def run(self):
         self._alive = True
         while self.alive:
             time.sleep(0.1)
+
+    @property
+    def quit(self):
+        return self._quit
 
     @property
     def alive(self):
@@ -122,6 +140,48 @@ class OnsetCanvas(object):
     def onset_times(self):
         return np.asarray(self.onset_data.time)
 
+    def shift_onsets(self, amount):
+        if self.has_onsets:
+            new_onsets = self.onset_data.copy() + amount
+            self.set_onset_data(new_onsets)
+
+    def modify_onsets_at_time(self, x, threshold=0.5, create=True):
+        """Delete any offsets that occur at time +/- a threshold.
+
+        Returns True if any are deleted, False otherwise.
+
+        Parameters
+        ----------
+        x : float
+            Time in seconds
+
+        threshold : float
+            In seconds.
+        """
+        od = None
+        # Debugging checks
+        if self.has_onsets:
+            if self.onset_times is None:
+                logger.error("Onset Times is None!")
+        if x is None:
+            logger.error("modify_onsets_at_time() - x is None!")
+        if threshold is None:
+            logger.error("modify_onsets_at_time() - Threshold is None!")
+
+        if (self.has_onsets and
+                (np.abs(self.onset_times - x) < threshold).any()):
+            # Collision! Remove it
+            idx = (np.abs(self.onset_times - x) < threshold).nonzero()[0]
+            logger.debug("Collision: {}".format(idx))
+            od = self.onset_data.drop(
+                pd.Index([self.onset_data.index[idx[0]]]))
+        # If there's no onset at this location and create mode is on.
+        elif create:
+            logger.debug("New datapoint!")
+            od = self.onset_data.append(dict(time=x), ignore_index=True)
+        if od is not None:
+            self.set_onset_data(od)
+
     def on_key_press(self, event):
         """Handle key-press events.
 
@@ -132,49 +192,98 @@ class OnsetCanvas(object):
             spacebar : Toggle a marker at the current mouse position.
             c : Clear all current onsets
         """
-        print('Received: ', event.key)
+        logger.debug('Received: ', event.key)
         sys.stdout.flush()
-        if event.key == 'x':
-            print("Saving to: {}".format(self.output_file))
+        if event.key == 'Q':
+            logger.info("Quitting")
+            plt.close()
+            self._alive = False
+            self._quit = True
+
+        elif event.key == 'x':
+            logger.info("Saving to: {}".format(self.output_file))
             self.save_onsets()
             plt.close()
             self._alive = False
 
         elif event.key == 'w':
-            print("Saving to: {}".format(self.output_file))
+            logger.info("Saving to: {}".format(self.output_file))
             self.save_onsets()
 
         elif event.key == 'q':
-            print("Closing")
+            logger.info("Closing")
+            plt.close()
+            self._alive = False
+            self._mark_for_later = True
+
+        elif event.key == 'c':
+            logger.info("Clearing existing markers")
+            self.clear_onsets()
+
+        elif event.key == 'm':
+            logger.info("Marking for later")
+            self._mark_for_later = True
             plt.close()
             self._alive = False
 
-        elif event.key == 'c':
-            print("Clearing existing markers")
-            self.clear_onsets()
-
         elif event.key == ' ':
             x, y = event.xdata, event.ydata
-            print('({:4}, {:4})'.format(x, y))
-            if self.has_onsets and (np.abs(self.onset_times - x) < 0.5).any():
-                # Collision! Remove it
-                idx = (np.abs(self.onset_data.time - x) < 0.5).nonzero()[0]
-                print("Collision: {}".format(idx))
-                od = self.onset_data.drop(
-                    pd.Index([self.onset_data.index[idx[0]]]))
-            else:
-                print("New datapoint!")
-                od = self.onset_data.append(dict(time=x), ignore_index=True)
-            self.set_onset_data(od)
+            logger.debug('({:4}, {:4})'.format(x, y))
+            self.modify_onsets_at_time(x)
+
+        elif event.key == 'd':
+            # Delete in a larger range (1s)
+            x, y = event.xdata, event.ydata
+            logger.info('({:4}, {:4})'.format(x, y))
+            self.modify_onsets_at_time(x, threshold=1.0, create=False)
+
+        elif event.key == 'D':
+            # Delete in an even larger range (5s)
+            x, y = event.xdata, event.ydata
+            logger.info('({:4}, {:4})'.format(x, y))
+            self.modify_onsets_at_time(x, threshold=5.0, create=False)
+
+        # Use onset detectors to get started
+        elif event.key == '1':
+            # Reset onsets with "envelope_onsets"
+            logger.debug("Getting envelope_onsets()")
+            onsets = S.envelope_onsets(self.x, self.fs)
+            self.set_onset_data(pd.DataFrame(dict(time=onsets)))
+
+        elif event.key == '2':
+            # Reset onsets with "logcqt_onsets"
+            logger.debug("Getting logcqt_onsets()")
+            onsets = S.logcqt_onsets(self.x, self.fs)
+            self.set_onset_data(pd.DataFrame(dict(time=onsets)))
+
+        elif event.key == 'left':
+            # Shift all markers to the left (subtract) by 10ms/.01s
+            self.shift_onsets(-.01)
+
+        elif event.key == 'right':
+            # Shift all markers to the left (subtract) by 10ms/.01s
+            self.shift_onsets(.01)
+
+        elif event.key == 'up':
+            # Shift all markers to the left (subtract) by 100ms/.1s
+            self.shift_onsets(-.1)
+
+        elif event.key == 'down':
+            # Shift all markers to the left (subtract) by 100ms/.1s
+            self.shift_onsets(.1)
 
 
 def annotate_one(audio_file, onset_file, output_file=None, title=None):
     if output_file is None:
         output_file = onset_file.replace(".csv", "-fix.csv")
 
+    if os.path.exists(output_file):
+        onset_file = output_file
+
     logger.info("Working on audio: {}".format(audio_file))
     logger.info("Onset_file: {}".format(onset_file))
     logger.info("Title: {}".format(title))
+    t0 = time.time()
 
     canvas = OnsetCanvas(audio_file, output_file, pd.read_csv(onset_file),
                          title=title)
@@ -182,7 +291,9 @@ def annotate_one(audio_file, onset_file, output_file=None, title=None):
     plt.show(block=True)
 
     # Should we continue?
-    return canvas.alive
+    t_end = time.time() - t0
+    logger.info("Took {}s to work on {}".format(t_end, audio_file))
+    return canvas.quit, canvas._mark_for_later
 
 
 if __name__ == '__main__':
@@ -194,6 +305,10 @@ if __name__ == '__main__':
         help=".")
     parser.add_argument(
         "--verbose", action='store_true', help="Set verbosity")
+    parser.add_argument(
+        '--startat', type=int)
+    parser.add_argument(
+        '--marked_file', default='marked_for_later_idx.txt')
 
     args = parser.parse_args()
     level = 'INFO' if not args.verbose else 'DEBUG'
@@ -201,14 +316,36 @@ if __name__ == '__main__':
 
     dframe = pd.read_csv(args.index_file)
 
+    marked_idxs = []
+    completed_idxs = []
+
     for idx, row in dframe.iterrows():
-        logger.debug("Annotating {}".format(row))
+        if args.startat and idx < args.startat:
+            continue
+
+        logger.info("Annotating {} [idx={}]".format(row, idx))
         # TODO(cbj): There should be a better way to handle this
         #  ... and/or a better naming scheme for the columns
         onsets = row.get('onset_file', row.get('logcqt', None))
         if onsets:
-            is_alive = annotate_one(row.audio_file, onsets, title=idx)
+            quit, marked = annotate_one(row.audio_file, onsets,
+                                        title="{} of {}".format(
+                                            idx, len(dframe)))
 
-            if not is_alive:
+            if quit:
                 logger.info("Application Exiting...")
                 break
+            if marked:
+                marked_idxs.append(idx)
+            else:
+                completed_idxs.append(idx)
+
+    print("The following indexes were marked to return to:")
+    pprint.pprint(marked_idxs)
+
+    with open(args.marked_file, 'a') as fh:
+        for index in marked_idxs:
+            fh.write("{}\n".format(index))
+
+    print("In this session, you completed {} files".format(len(completed_idxs))
+    print("In this session, you marked {} files".format(len(marked_idxs)))
