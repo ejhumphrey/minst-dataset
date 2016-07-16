d7f125f6d710b9a1afd0be2b70abcae8cbf236bd
Christopher Jacoby <cjacoby@museami.com>
2016-07-16 00:44:47 -0700

    modifications to annotate to make it easier to do manual annotations.

--- scripts/annotate.py
+++ scripts/annotate.py
@@ -14,6 +14,7 @@ Keys:
 from __future__ import print_function
 import argparse
 import claudio
+import librosa
 import logging
 import matplotlib
 import numpy as np
@@ -38,11 +39,11 @@ class OnsetCanvas(object):
                  nhop=100, group=None, target=None, title=None,
                  verbose=None):
 
-        self.fig, self.axes = plt.subplots(nrows=2, ncols=1,
-                                           figsize=(16, 6))
+        self.fig, self.axes = plt.subplots(nrows=3, ncols=1,
+                                           figsize=(20, 6))
 
         self.x, self.fs = claudio.read(audio_file, samplerate=22050,
-                             channels=1, bytedepth=2)
+                                       channels=1, bytedepth=2)
 
         onset_data = pd.DataFrame([]) if onset_data is None else onset_data
         self.output_file = output_file
@@ -50,16 +51,20 @@ class OnsetCanvas(object):
         self.trange = np.arange(0, len(self.x), nhop) / float(self.fs)
         self.waveform = self.x.flatten()[::nhop]
         self.envelope = S.log_envelope(self.x, self.fs, nhop)[::nhop]
-        # self.onset_data = onset_data
+        self.lcqt = S.logcqt(self.x, self.fs)
+
+        # self.onset_data = set_onset_data
         self.wave_handle = self.axes[0].plot(self.trange, self.waveform)
         self.env_handle = self.axes[1].plot(self.trange, self.envelope)
+        self.lcqt_handle = self.axes[2].imshow(self.lcqt,
+            aspect='auto', origin='lower', interpolation='nearest')
         self.onset_handles = []
         self.refresh_xlim()
 
         title = '' if not title else title
         title = ("{}\nx: write and close / w: write / q: close / c: clear / "
-            "SPACE: set/delete marker\n"
-            "1: envelope onsets / 2: logcqt onsets".format(title))
+                 "SPACE: set/delete marker\n"
+                 "1: envelope onsets / 2: logcqt onsets".format(title))
         self.axes[0].set_title(title)
 
         self.set_onset_data(onset_data)
@@ -114,21 +119,21 @@ class OnsetCanvas(object):
         self.onset_handles = []
         logger.debug("drawing lines : {}".format(self.onset_times))
         self.onset_handles += [self.axes[0].vlines(
-            self.onset_data.time, ymin=-1.05*self.x_max,
-            ymax=1.05*self.x_max, color='k', alpha=0.5, linewidth=3)]
+            self.onset_data.time, ymin=-1.05 * self.x_max,
+            ymax=1.05 * self.x_max, color='k', alpha=0.5, linewidth=3)]
         for t, i in zip(self.onset_data.time, self.onset_data.index):
             self.onset_handles += [self.axes[0].text(
                 x=t, y=self.x_max, s=i, va='top', ha='left', fontsize=16)]
 
         self.onset_handles += [self.axes[1].vlines(
-            self.onset_data.time, ymin=self.envelope.min()*1.05,
+            self.onset_data.time, ymin=self.envelope.min() * 1.05,
             ymax=0, color='k', alpha=0.5, linewidth=3)]
         for t, i in zip(self.onset_data.time, self.onset_data.index):
             self.onset_handles += [self.axes[1].text(
                 x=t, y=-3, s=i, va='top', ha='left', fontsize=16)]
 
     def refresh_xlim(self):
-        for ax in self.axes:
+        for ax in self.axes[:-1]:
             ax.set_xlim(0, self.trange.max())
             ax.set_xlabel("Time (sec)")
 
@@ -192,7 +197,7 @@ class OnsetCanvas(object):
             spacebar : Toggle a marker at the current mouse position.
             c : Clear all current onsets
         """
-        logger.debug('Received: ', event.key)
+        logger.debug('Received: {}'.format(event.key))
         sys.stdout.flush()
         if event.key == 'Q':
             logger.info("Quitting")
@@ -244,45 +249,47 @@ class OnsetCanvas(object):
 
         # Use onset detectors to get started
         elif event.key == '1':
-            # Reset onsets with "envelope_onsets"
-            logger.debug("Getting envelope_onsets()")
-            onsets = S.envelope_onsets(self.x, self.fs, wait=int(self.fs*.001))
+            logger.debug("Getting envelope_onsets(.008)")
+            onsets = S.envelope_onsets(self.x, self.fs,
+                                       wait=int(self.fs * .008))
             self.set_onset_data(pd.DataFrame(dict(time=onsets)))
 
         elif event.key == '2':
             # Reset onsets with "envelope_onsets"
-            logger.debug("Getting envelope_onsets()")
-            onsets = S.envelope_onsets(self.x, self.fs, wait=int(self.fs*.005))
+            logger.debug("Getting envelope_onsets(.01)")
+            onsets = S.envelope_onsets(self.x, self.fs,
+                                       wait=int(self.fs * .01))
             self.set_onset_data(pd.DataFrame(dict(time=onsets)))
 
         elif event.key == '3':
             # Reset onsets with "envelope_onsets"
-            logger.debug("Getting envelope_onsets()")
-            onsets = S.envelope_onsets(self.x, self.fs, wait=int(self.fs*.01))
+            logger.debug("Getting envelope_onsets(.02)")
+            onsets = S.envelope_onsets(self.x, self.fs,
+                                       wait=int(self.fs * .02))
             self.set_onset_data(pd.DataFrame(dict(time=onsets)))
 
         elif event.key == '4':
             # Reset onsets with "envelope_onsets"
-            logger.debug("Getting envelope_onsets()")
-            onsets = S.envelope_onsets(self.x, self.fs, wait=int(self.fs*.05))
+            logger.debug("Getting envelope_onsets(.05)")
+            onsets = S.envelope_onsets(self.x, self.fs,
+                                       wait=int(self.fs * .05))
             self.set_onset_data(pd.DataFrame(dict(time=onsets)))
 
         elif event.key == '5':
-            # Reset onsets with "envelope_onsets"
-            logger.debug("Getting envelope_onsets()")
-            onsets = S.envelope_onsets(self.x, self.fs, wait=int(self.fs*.075))
-            self.set_onset_data(pd.DataFrame(dict(time=onsets)))
+            pass
 
         elif event.key == '6':
-            # Reset onsets with "envelope_onsets"
-            logger.debug("Getting envelope_onsets()")
-            onsets = S.envelope_onsets(self.x, self.fs, wait=int(self.fs*.1))
+            # Reset onsets with "logcqt_onsets"
+            logger.debug("Getting logcqt_onsets()")
+            onsets = S.logcqt_onsets(self.x, self.fs,
+                                     wait=int(self.fs * .01))
             self.set_onset_data(pd.DataFrame(dict(time=onsets)))
 
         elif event.key == '7':
             # Reset onsets with "logcqt_onsets"
             logger.debug("Getting logcqt_onsets()")
-            onsets = S.logcqt_onsets(self.x, self.fs)
+            onsets = S.logcqt_onsets(self.x, self.fs,
+                                     wait=int(self.fs * .02))
             self.set_onset_data(pd.DataFrame(dict(time=onsets)))
 
         elif event.key == 'left':
@@ -350,6 +357,10 @@ if __name__ == '__main__':
         action='store_true',
         help="Don't look at files that don't have an assciated instrument "
              "in the index_file")
+    parser.add_argument(
+        '--random',
+        action='store_true',
+        help="Choose a random file.")
 
     args = parser.parse_args()
     level = 'INFO' if not args.verbose else 'DEBUG'
@@ -359,41 +370,58 @@ if __name__ == '__main__':
     if args.ignore_no_instrument:
         dframe = dframe.loc[dframe['instrument'].dropna().index]
 
-    marked_idxs = []
-    completed_idxs = []
+    if not args.random:
+        marked_idxs = []
+        completed_idxs = []
 
-    count = 0
-    for idx, row in dframe.iterrows():
-        if args.startat and idx < args.startat:
-            continue
+        count = 0
+        for idx, row in dframe.iterrows():
+            if args.startat and idx < args.startat:
+                continue
 
-        logger.info("Annotating:\n{} [idx={}]".format(row, idx))
-        # TODO(cbj): There should be a better way to handle this
-        #  ... and/or a better naming scheme for the columns
-        onsets = row.get('onset_file', row.get('logcqt', None))
-        quit, marked = annotate_one(row.audio_file, onsets,
-                                    skip_existing=args.skip_existing,
-                                    title="{} of {} | instrument: {}"
-                                    .format(count, len(dframe),
-                                            row['instrument']))
+            logger.info("Annotating:\n{} [idx={}]".format(row, idx))
+            # TODO(cbj): There should be a better way to handle this
+            #  ... and/or a better naming scheme for the columns
+            onsets = row.get('onset_file', row.get('logcqt', None))
+            quit, marked = annotate_one(row.audio_file, onsets,
+                                        skip_existing=args.skip_existing,
+                                        title="{} of {} | instrument: {}"
+                                        .format(count, len(dframe),
+                                                row['instrument']))
 
-        if quit:
-            logger.info("Application Exiting...")
-            break
-        if marked:
-            marked_idxs.append(idx)
-        else:
-            completed_idxs.append(idx)
-        count += 1
+            if quit:
+                logger.info("Application Exiting...")
+                break
+            if marked:
+                marked_idxs.append(idx)
+            else:
+                completed_idxs.append(idx)
+            count += 1
 
-    print("The following indexes were marked to return to:")
-    pprint.pprint(marked_idxs)
+        print("The following indexes were marked to return to:")
+        pprint.pprint(marked_idxs)
 
-    with open(args.marked_file, 'a') as fh:
-        for index in marked_idxs:
-            fh.write("{}\n".format(index))
+        with open(args.marked_file, 'a') as fh:
+            for index in marked_idxs:
+                fh.write("{}\n".format(index))
 
-    print("In this session, you completed {} files".format(
-        len(completed_idxs)))
-    print("In this session, you marked {} files".format(
-        len(marked_idxs)))
+        print("In this session, you completed {} files".format(
+            len(completed_idxs)))
+        print("In this session, you marked {} files".format(
+            len(marked_idxs)))
+
+    else:
+        while True:
+            row = dframe.sample()
+
+            logger.info("Annotating:\n{}".format(row))
+            # TODO(cbj): There should be a better way to handle this
+            #  ... and/or a better naming scheme for the columns
+            onsets = row.get('onset_file', row.get('logcqt', None))
+            quit, marked = annotate_one(row.audio_file.iloc[0], onsets.iloc[0],
+                                        skip_existing=False,
+                                        title="{}| instrument: {}"
+                                        .format(row.index[0], row['instrument'].iloc[0]))
+            if quit:
+                logger.info("Application Exiting...")
+                break
